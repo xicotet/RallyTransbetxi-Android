9ae1406ed35f2c3c1d25e1c9de15a1c535a278a1
Pablo Cano <pablocanofranch@gmail.com>
2024-02-19 14:55:16 +0100

    Add firestone collections and documents

--- app/src/main/java/com/canolabs/rallytransbetxi/MainActivity.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.canolabs.rallytransbetxi
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +12,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.canolabs.rallytransbetxi.ui.theme.RallyTransbetxiTheme
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : ComponentActivity() {
+
+    private val TAG = "FIREBASE_DATABASE"
+    private val db = FirebaseFirestore.getInstance()
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,6 +29,16 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Navigation()
+                    val stages = hashMapOf(
+                        "TC3" to "La Mina",
+                        "TC4" to "Sagrat Cor - Rodaor"
+                    )
+
+                    db.collection("stages").document("TCP").set(stages).addOnSuccessListener {
+                        Log.d(TAG, "DocumentSnapshot added")
+                    }.addOnFailureListener {
+                        Log.w(TAG, "Error adding document", it)
+                    }
                 }
             }
         }
