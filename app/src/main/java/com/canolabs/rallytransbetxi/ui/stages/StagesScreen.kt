0610db4ef666c9e162f7ee20a630687a149ea6f6
Pablo Cano <pablocanofranch@gmail.com>
2024-02-23 12:27:24 +0100

    Update StagesScreen to group stages by date

--- app/src/main/java/com/canolabs/rallytransbetxi/ui/stages/StagesScreen.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/stages/StagesScreen.kt
@@ -1,16 +1,21 @@
 package com.canolabs.rallytransbetxi.ui.stages
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.canolabs.rallytransbetxi.data.models.responses.Stage
+import com.canolabs.rallytransbetxi.ui.theme.PaddingRegular
+import com.canolabs.rallytransbetxi.ui.theme.robotoFamily
+import com.canolabs.rallytransbetxi.utils.DateTimeUtils
 
 @Composable
 fun StagesScreen(viewModel: StagesScreenViewModel) {
@@ -32,9 +37,25 @@ fun StagesScreen(viewModel: StagesScreenViewModel) {
 @Composable
 fun StageList(stages: List<Stage>) {
     val sortedStagesByStartTime = stages.sortedBy { it.startTime }
+    val groupedStagesByDate = sortedStagesByStartTime.groupBy {
+        it.startTime?.let { timestamp ->
+            DateTimeUtils.secondsToDateInSpanish(timestamp.seconds)
+        }
+    }
+
     LazyColumn {
-        items(sortedStagesByStartTime) { stage ->
-            StageCard(stage = stage)
+        groupedStagesByDate.entries.forEach { entry ->
+            item {
+                Text(
+                    text = entry.key ?: "",
+                    style = MaterialTheme.typography.labelLarge,
+                    fontFamily = robotoFamily,
+                    modifier = Modifier.padding(PaddingRegular)
+                )
+            }
+            items(entry.value) { stage ->
+                StageCard(stage = stage)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/canolabs/rallytransbetxi/utils/DateTimeUtils.kt
@@ -0,0 +1,154 @@
+package com.canolabs.rallytransbetxi.utils
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+object DateTimeUtils {
+
+    const val DEFAULT_FROM_TIME = "00:00"
+    const val DEFAULT_TO_TIME = "23:59"
+
+    private const val DATE_FORMAT = "dd/MM/yyyy"
+    private const val API_DATE_FORMAT = "yyyy-MM-dd"
+    private const val TIME_FORMAT = "HH:mm"
+
+    private val dateFormatter = SimpleDateFormat(DATE_FORMAT, Locale.getDefault())
+    private val apiDateFormatter = SimpleDateFormat(API_DATE_FORMAT, Locale.getDefault())
+    private val timeFormatter = SimpleDateFormat(TIME_FORMAT, Locale.getDefault())
+
+    fun secondsToDateInSpanish(seconds: Long): String {
+        val date = Date(seconds * 1000) // Convert seconds to milliseconds
+        val format = SimpleDateFormat("EEEE d 'de' MMMM 'de' yyyy", Locale("es", "ES"))
+        val dateString = format.format(date)
+        return dateString[0].uppercaseChar() + dateString.substring(1)
+    }
+
+    fun getCurrentDateTime(): String {
+        val currentDateTime = Calendar.getInstance().time
+        return dateFormatter.format(currentDateTime) + " " + timeFormatter.format(currentDateTime)
+    }
+
+    fun getCurrentDate(): String {
+        val currentDate = Calendar.getInstance()
+        return dateFormatter.format(currentDate.time)
+    }
+
+    fun getCurrentTime(): String {
+        val currentTime = Calendar.getInstance()
+        return timeFormatter.format(currentTime.time)
+    }
+
+    fun getCurrentYear(): Int {
+        return Calendar.getInstance().get(Calendar.YEAR)
+    }
+
+    fun getCurrentMonth(): Int {
+        return Calendar.getInstance().get(Calendar.MONTH)
+    }
+
+    fun getCurrentDay(): Int {
+        return Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+    }
+
+    fun getCurrentHour(): Int {
+        val calendar = Calendar.getInstance()
+        return calendar.get(Calendar.HOUR_OF_DAY)
+    }
+
+    fun getCurrentMinute(): Int {
+        val calendar = Calendar.getInstance()
+        return calendar.get(Calendar.MINUTE)
+    }
+
+    // Returns true if time2 is greater than time1
+    fun strictlyBeforeTime(time1: String, time2: String): Boolean {
+        val parsedTime1 = parseTime(time1)
+        val parsedTime2 = parseTime(time2)
+        return if (parsedTime1 != null && parsedTime2 != null) {
+            parsedTime2 > parsedTime1
+        } else {
+            false
+        }
+    }
+
+    // Returns true if date2 is greater or equals than date1
+    fun beforeDate(date1: String, date2: String): Boolean {
+        val parsedDate1 = parseDate(date1)
+        val parsedDate2 = parseDate(date2)
+        return if (parsedDate1 != null && parsedDate2 != null) {
+            parsedDate2 >= parsedDate1
+        } else {
+            false
+        }
+    }
+
+    fun addDaysToDate(date: String, days: Int): String {
+        val parsedDate = dateFormatter.parse(date)
+        val calendar = Calendar.getInstance()
+        if (parsedDate != null) {
+            calendar.time = parsedDate
+        }
+        calendar.add(Calendar.DATE, days)
+        return dateFormatter.format(calendar.time)
+    }
+
+    fun addHoursToTime(time: String, hours: Int): String {
+        val parsedTime = timeFormatter.parse(time)
+        val calendar = Calendar.getInstance()
+        if (parsedTime != null) {
+            calendar.time = parsedTime
+        }
+        calendar.add(Calendar.HOUR_OF_DAY, hours)
+        return timeFormatter.format(calendar.time)
+    }
+
+    fun parseDate(date: String): Long? {
+        try {
+            return dateFormatter.parse(date)?.time
+        } catch (e: ParseException) {
+            return null
+        }
+    }
+
+    fun parseDateToUtcMillis(date: String): Long? {
+        val parsedDate = parseDate(date) ?: return null
+        return parsedDate + TimeZone.getDefault().getOffset(parsedDate)
+    }
+
+    fun parseTime(time: String): Long? {
+        try {
+            return timeFormatter.parse(time)?.time
+        } catch (e: ParseException) {
+            return null
+        }
+    }
+
+    fun formatDate(date: Long): String? {
+        try {
+            return dateFormatter.format(Date(date))
+        } catch (e: ParseException) {
+            return null
+        }
+    }
+
+    fun formatToApiDateStandard(date: String): String? {
+        return try {
+            dateFormatter.parse(date)?.let { apiDateFormatter.format(it) }
+        } catch (e: ParseException) {
+            null
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun formatApiDateToCustomFormat(apiDate: String): String {
+        val apiFormatter = DateTimeFormatter.ofPattern(API_DATE_FORMAT)
+        val customFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT)
+        val customDate = LocalDate.parse(apiDate , apiFormatter)
+        return customDate.format(customFormatter)
+    }
+}
