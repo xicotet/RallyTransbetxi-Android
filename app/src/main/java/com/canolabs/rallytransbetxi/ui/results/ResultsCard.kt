ed01c27acff1bd2e1e441c43fab8548d96cd40a6
Pablo Cano <pablocanofranch@gmail.com>
2024-02-27 22:23:51 +0100

    Update card layout to each result item

--- app/src/main/java/com/canolabs/rallytransbetxi/ui/results/ResultsCard.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/results/ResultsCard.kt
@@ -1,47 +1,100 @@
 package com.canolabs.rallytransbetxi.ui.results
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.canolabs.rallytransbetxi.R
+import androidx.compose.material3.Icon
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.ui.Alignment
 import com.canolabs.rallytransbetxi.data.models.responses.Result
+import com.canolabs.rallytransbetxi.ui.theme.ezraFamily
 import com.canolabs.rallytransbetxi.ui.theme.robotoFamily
 
 @Composable
 fun ResultCard(result: Result) {
     Card(
         modifier = Modifier
+            .padding(16.dp)
             .fillMaxWidth()
-            .height(150.dp)
-            .padding(8.dp),
+            .height(150.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+        shape = RoundedCornerShape(8.dp)
     ) {
+
         Row(
             modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
+            // Place number 1
             Text(
-                text = result.team.name,
-                style = MaterialTheme.typography.bodyMedium,
-                fontFamily = robotoFamily
+                text = "1",
+                fontFamily = ezraFamily,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier
+                    .weight(0.15f)
+                    .padding(start = 8.dp)
             )
-            Text(
-                text = "Score: ${result.team.driver}",
-                style = MaterialTheme.typography.bodySmall,
-                fontFamily = robotoFamily
-            )
-            Text(
-                text = "Position: ${result.team.codriver}",
-                style = MaterialTheme.typography.bodySmall,
-                fontFamily = robotoFamily
+
+            VerticalDivider(thickness = 4.dp, color = MaterialTheme.colorScheme.primary)
+
+            // Column with three Texts
+            Column(
+                modifier = Modifier
+                    .weight(0.8f)
+                    .padding(start = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = result.team.driver.substringBeforeLast(" "),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Text(
+                    text = result.team.codriver.substringBeforeLast(" "),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row (
+                    modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painterResource(id = R.drawable.timelapse_outlined),
+                        contentDescription = null
+                    )
+                    Text(
+                        text = result.time,
+                        fontFamily = robotoFamily,
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
+
+            // Image on the right side
+            Icon(
+                painterResource(id = R.drawable.group_filled),
+                contentDescription = null
             )
         }
     }
