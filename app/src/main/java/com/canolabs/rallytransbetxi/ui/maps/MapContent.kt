c723de44a83c94f7e6df9452dcd71ddedb5f08db
Pablo Cano <pablocanofranch@gmail.com>
2024-04-05 12:28:10 +0200

    Refactor maps screen to split composable into two

---
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/maps/MapContent.kt
@@ -0,0 +1,146 @@
+package com.canolabs.rallytransbetxi.ui.maps
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.canolabs.rallytransbetxi.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.Polyline
+import kotlinx.coroutines.delay
+
+@Composable
+fun MapContent(
+    state: MapsScreenUIState,
+    cameraPositionState: CameraPositionState,
+    betxi: LatLng,
+    scaffoldPadding: PaddingValues
+) {
+    Column {
+        if (state.isLoading) {
+            LinearProgressIndicator(
+                // dynamic progress value
+                modifier = Modifier
+                    .fillMaxWidth() // fill the width of the parent
+                    .padding(scaffoldPadding), // add some padding
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            )
+        }
+
+        Box(modifier = Modifier.fillMaxSize()) {
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                uiSettings = state.uiSettings,
+                cameraPositionState = cameraPositionState
+            ) {
+                Polyline(
+                    points = state.stage.geoPoints?.map {
+                        LatLng(it.latitude, it.longitude)
+                    } ?: emptyList(),
+                    color = MaterialTheme.colorScheme.primary
+                )
+                Marker(
+                    state = MarkerState(betxi),
+                    title = state.stage.name,
+                )
+
+                // Animate the camera to the first geo point of the stage
+                state.stage.geoPoints?.first()?.let { geoPoint ->
+                    val targetPosition = LatLng(geoPoint.latitude, geoPoint.longitude)
+                    val cameraUpdate = CameraUpdateFactory.newLatLngZoom(targetPosition, 15f)
+                    LaunchedEffect(cameraUpdate) {
+                        delay(500)
+                        cameraPositionState.animate(cameraUpdate, durationMs = 2000)
+                    }
+                }
+            }
+
+            AssistChip(
+                onClick = { /*TODO*/ },
+                label = { Text(text = stringResource(id = R.string.results)) },
+                leadingIcon = {
+                    Icon(
+                        painterResource(id = R.drawable.sports_score),
+                        contentDescription = null
+                    )
+                },
+                colors = AssistChipDefaults.assistChipColors().copy(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    labelColor = MaterialTheme.colorScheme.onBackground
+                ),
+                modifier = Modifier
+                    .padding(scaffoldPadding)
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+
+            Surface(
+                shadowElevation = 4.dp,
+                shape = CircleShape,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp, 16.dp, 16.dp, 200.dp)
+                    .background(Color.White, CircleShape)
+            ) {
+                IconButton(
+                    onClick = { /*TODO: Handle MyLocation button click*/ },
+                    modifier = Modifier.size(60.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.my_location_unknown),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.error,
+                        modifier = Modifier.size(36.dp)
+                    )
+                }
+            }
+
+            Surface(
+                shadowElevation = 4.dp,
+                shape = CircleShape,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp, 16.dp, 16.dp, 120.dp)
+                    .background(Color.White, CircleShape)
+            ) {
+                IconButton(
+                    onClick = {  },
+                    modifier = Modifier.size(60.dp),
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.directions_outlined),
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        modifier = Modifier.size(36.dp)
+                    )
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/canolabs/rallytransbetxi/ui/maps/MapsScreen.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/maps/MapsScreen.kt
@@ -3,25 +3,17 @@ package com.canolabs.rallytransbetxi.ui.maps
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.AssistChip
-import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -29,29 +21,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.canolabs.rallytransbetxi.R
 import com.canolabs.rallytransbetxi.ui.miscellaneous.Shimmer
 import com.canolabs.rallytransbetxi.ui.theme.ezraFamily
 import com.canolabs.rallytransbetxi.utils.Constants
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
-import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
-import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -128,106 +109,11 @@ fun MapsScreen(
             )
         },
     ) {
-        Column {
-            if (state.isLoading) {
-                LinearProgressIndicator(
-                    // dynamic progress value
-                    modifier = Modifier
-                        .fillMaxWidth() // fill the width of the parent
-                        .padding(it), // add some padding
-                    color = MaterialTheme.colorScheme.secondary,
-                    trackColor = MaterialTheme.colorScheme.surfaceVariant,
-                )
-            }
-
-            Box(modifier = Modifier.fillMaxSize()) {
-                GoogleMap(
-                    modifier = Modifier.fillMaxSize(),
-                    uiSettings = state.uiSettings,
-                    cameraPositionState = cameraPositionState
-                ) {
-                    Polyline(
-                        points = state.stage.geoPoints?.map {
-                            LatLng(it.latitude, it.longitude)
-                        } ?: emptyList(),
-                        color = MaterialTheme.colorScheme.primary
-                    )
-                    Marker(
-                        state = MarkerState(betxi),
-                        title = state.stage.name,
-                    )
-
-                    // Animate the camera to the first geo point of the stage
-                    state.stage.geoPoints?.first()?.let { geoPoint ->
-                        val targetPosition = LatLng(geoPoint.latitude, geoPoint.longitude)
-                        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(targetPosition, 15f)
-                        LaunchedEffect(cameraUpdate) {
-                            delay(500)
-                            cameraPositionState.animate(cameraUpdate, durationMs = 2000)
-                        }
-                    }
-                }
-
-                AssistChip(
-                    onClick = { /*TODO*/ },
-                    label = { Text(text = stringResource(id = R.string.results))},
-                    leadingIcon = {
-                        Icon(
-                            painterResource(id = R.drawable.sports_score),
-                            contentDescription = null
-                        )
-                    },
-                    colors = AssistChipDefaults.assistChipColors().copy(
-                        containerColor = MaterialTheme.colorScheme.background,
-                        labelColor = MaterialTheme.colorScheme.onBackground
-                    ),
-                    modifier = Modifier
-                        .padding(it)
-                        .padding(horizontal = 16.dp, vertical = 8.dp)
-                )
-
-                Surface(
-                    shadowElevation = 4.dp,
-                    shape = CircleShape,
-                    modifier = Modifier
-                        .align(Alignment.BottomEnd)
-                        .padding(16.dp, 16.dp, 16.dp, 200.dp)
-                        .background(Color.White, CircleShape)
-                ) {
-                    IconButton(
-                        onClick = { /*TODO: Handle MyLocation button click*/ },
-                        modifier = Modifier.size(60.dp),
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.my_location_unknown),
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.error,
-                            modifier = Modifier.size(36.dp)
-                        )
-                    }
-                }
-
-                Surface(
-                    shadowElevation = 4.dp,
-                    shape = CircleShape,
-                    modifier = Modifier
-                        .align(Alignment.BottomEnd)
-                        .padding(16.dp, 16.dp, 16.dp, 120.dp)
-                        .background(Color.White, CircleShape)
-                ) {
-                    IconButton(
-                        onClick = { /*TODO: Handle MyLocation button click*/ },
-                        modifier = Modifier.size(60.dp),
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.directions_outlined),
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.onSurface,
-                            modifier = Modifier.size(36.dp)
-                        )
-                    }
-                }
-            }
-        }
+        MapContent(
+            state = state,
+            cameraPositionState = cameraPositionState,
+            betxi = betxi,
+            scaffoldPadding = it
+        )
     }
 }
