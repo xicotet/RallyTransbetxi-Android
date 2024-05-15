b3b7a7b36c2b6825ccaf6ceadf64c95208f25d7c
Pablo Cano <pablocanofranch@gmail.com>
2024-05-15 10:57:29 +0200

    Update stages results card to reduce number of elements

--- app/src/main/java/com/canolabs/rallytransbetxi/ui/results/StagesResultsCard.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/results/StagesResultsCard.kt
@@ -1,7 +1,6 @@
 package com.canolabs.rallytransbetxi.ui.results
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -14,7 +13,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -23,15 +21,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.canolabs.rallytransbetxi.R
 import com.canolabs.rallytransbetxi.data.models.responses.Stage
-import com.canolabs.rallytransbetxi.ui.theme.antaFamily
 import com.canolabs.rallytransbetxi.ui.theme.cardsElevation
 import com.canolabs.rallytransbetxi.ui.theme.robotoFamily
-import com.canolabs.rallytransbetxi.utils.DateTimeUtils
 
 @Composable
 fun StagesResultsCard(
@@ -64,59 +58,22 @@ fun StagesResultsCard(
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
-            // Column of two texts
+            // Column with the stage acronym
             Column(
                 modifier = Modifier
-                    .weight(0.6f)
                     .padding(horizontal = 16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+                horizontalAlignment = Alignment.Start,
             ) {
                 Text(
                     text = stage.acronym,
                     fontFamily = robotoFamily,
                     style = MaterialTheme.typography.headlineMedium,
                     color = MaterialTheme.colorScheme.onSecondaryContainer,
-                    modifier = Modifier
-                        .padding(start = 8.dp)
+                    textAlign = TextAlign.Center,
                 )
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Text(
-                    text = stage.startTime?.let {
-                        DateTimeUtils.secondsToDateInSpanishAbbreviated(it.seconds)
-                    }.toString(),
-                    fontFamily = robotoFamily,
-                    style = MaterialTheme.typography.headlineMedium,
-                    color = MaterialTheme.colorScheme.onSecondaryContainer,
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                )
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Row (
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Icon(
-                        painterResource(id = R.drawable.schedule_filled),
-                        contentDescription = null
-                    )
-
-                    Spacer(modifier = Modifier.width(4.dp))
-
-                    Text(
-                        text = DateTimeUtils.formatTimeFromSeconds(stage.startTime?.seconds ?: 0),
-                        fontFamily = antaFamily,
-                        fontWeight = FontWeight.Bold,
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                }
             }
+
+            Spacer(modifier = Modifier.width(16.dp))
 
             VerticalDivider(
                 modifier = Modifier
@@ -127,7 +84,7 @@ fun StagesResultsCard(
 
             Column(
                 modifier = Modifier
-                    .weight(1f)
+                    .fillMaxWidth()
                     .padding(start = 16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
