748f8e57c71530a1740dc15b0487c804d21e6e8e
Pablo Cano <pablocanofranch@gmail.com>
2024-05-17 18:01:20 +0200

    Fix global results tab ordering results by search bar

--- app/src/main/java/com/canolabs/rallytransbetxi/ui/results/GlobalResultsTab.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/results/GlobalResultsTab.kt
@@ -17,18 +17,7 @@ fun GlobalResultsTab(
             ResultsCardShimmer()
         }
     } else {
-        val filteredResultsBySearchBar = if (state.isSearchBarVisible) {
-            results.filter { result ->
-                result.team.driver.contains(state.searchText, ignoreCase = true) ||
-                    result.team.codriver.contains(state.searchText, ignoreCase = true) ||
-                    result.team.name.contains(state.searchText, ignoreCase = true) ||
-                    result.time.contains(state.searchText, ignoreCase = true)
-            }
-        } else {
-            results
-        }
-
-        val filteredResultsByCategory = filteredResultsBySearchBar.filter { result ->
+        val filteredResultsByCategory = results.filter { result ->
             state.selectedRacingCategories.any { selectedCategory ->
                 result.team.category.name == stringResource(id = selectedCategory.getName())
             }
@@ -36,10 +25,22 @@ fun GlobalResultsTab(
 
         val sortedResultsByTime = filteredResultsByCategory.sortedBy { it.time }
 
-        // We can not use LazyColumn here because we have set up
-        // a vertical scrollable component in main function
+        val filteredResultsBySearchBar = if (state.isSearchBarVisible) {
+            sortedResultsByTime.filter { result ->
+                result.team.driver.contains(state.searchText, ignoreCase = true) ||
+                    result.team.codriver.contains(state.searchText, ignoreCase = true) ||
+                    result.team.name.contains(state.searchText, ignoreCase = true) ||
+                    result.team.number.contains(state.searchText, ignoreCase = true) ||
+                    result.time.contains(state.searchText, ignoreCase = true)
+            }
+        } else {
+            sortedResultsByTime
+        }
+
         sortedResultsByTime.forEachIndexed { index, result ->
-            ResultCard(result = result, position = index + 1)
+            if (filteredResultsBySearchBar.contains(result)) {
+                ResultCard(result = result, position = index + 1)
+            }
         }
     }
 }
