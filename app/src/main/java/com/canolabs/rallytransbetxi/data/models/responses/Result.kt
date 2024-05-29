105c792c7e41f0961052b7d470a8616a3c09bdbd
Pablo Cano <pablocanofranch@gmail.com>
2024-05-29 18:06:56 +0200

    Add disqualified property to results card

--- app/src/main/java/com/canolabs/rallytransbetxi/data/models/responses/Result.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/data/models/responses/Result.kt
@@ -3,5 +3,6 @@ package com.canolabs.rallytransbetxi.data.models.responses
 data class Result(
     val number: String = "",
     var team: Team = Team(),
-    val time: String = ""
+    val time: String = "",
+    val disqualified: Boolean = false,
 )
--- app/src/main/java/com/canolabs/rallytransbetxi/ui/results/ResultsCard.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/results/ResultsCard.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -42,162 +43,176 @@ fun ResultCard(
     result: Result,
     position: Int
 ) {
-    Card(
-        modifier = Modifier
-            .padding(horizontal = 16.dp, vertical = 12.dp)
-            .fillMaxWidth(),
-        elevation = CardDefaults.cardElevation(defaultElevation = cardsElevation),
-        shape = RoundedCornerShape(8.dp)
-    ) {
-
-        val gradient = Brush.linearGradient(
-            colors = listOf(
-                MaterialTheme.colorScheme.primaryContainer,
-                MaterialTheme.colorScheme.primary.copy(alpha = 0.25f)
-            ),
-            start = Offset(0f, 0f), // Start at the top left corner
-            end = Offset(1000f, 1000f)
-        )
-
-        Row(
+    Column {
+        if (result.disqualified) {
+            Text(
+                text = stringResource(id = R.string.disqualified),
+                fontFamily = antaFamily,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.error,
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 8.dp)
+            )
+        }
+        Card(
             modifier = Modifier
-                .background(brush = gradient)
-                .fillMaxWidth()
-                .height(IntrinsicSize.Min),
-            verticalAlignment = Alignment.CenterVertically
+                .padding(horizontal = 16.dp, vertical = 12.dp)
+                .fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(defaultElevation = cardsElevation),
+            shape = RoundedCornerShape(8.dp)
         ) {
-            Column {
-                Box(
-                    modifier = Modifier.fillMaxHeight()
-                ) {
-                    Text(
-                        text = position.toString(),
-                        fontFamily = antaFamily,
-                        style = MaterialTheme.typography.headlineMedium,
-                        color = MaterialTheme.colorScheme.onPrimaryContainer,
-                        modifier = Modifier
-                            .padding(start = 8.dp)
-                            .align(Alignment.Center)
-                    )
 
-                    Text(
-                        text = result.team.category.categoryId.substring(0, 5).uppercase(),
-                        style = MaterialTheme.typography.bodyMedium,
-                        fontFamily = antaFamily,
-                        color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.5f),
-                        modifier = Modifier
-                            .padding(start = 8.dp)
-                            .align(Alignment.BottomCenter),
-                    )
-                }
-            }
+            val gradient = Brush.linearGradient(
+                colors = listOf(
+                    MaterialTheme.colorScheme.primaryContainer,
+                    MaterialTheme.colorScheme.primary.copy(alpha = 0.25f)
+                ),
+                start = Offset(0f, 0f), // Start at the top left corner
+                end = Offset(1000f, 1000f)
+            )
 
-            /*VerticalDivider(
+            Row(
                 modifier = Modifier
-                    .fillMaxHeight()
-                    .width(4.dp)
-                    .padding(horizontal = 8.dp),
-                thickness = 4.dp,
-                color = MaterialTheme.colorScheme.primary
-            )*/
-
-            // Column with three Texts
-            Column(
-                modifier = Modifier
-                    .weight(0.8f)
-                    .padding(start = 8.dp, top = 16.dp, end = 8.dp, bottom = 16.dp),
-                horizontalAlignment = Alignment.CenterHorizontally,
+                    .background(brush = gradient)
+                    .fillMaxWidth()
+                    .height(IntrinsicSize.Min),
+                verticalAlignment = Alignment.CenterVertically
             ) {
-                // To peacefully accommodate german names which only consists of two words
-                val driverName = result.team.driver
-                val trimmedDriverName = if (driverName.split(" ").size > 2) {
-                    driverName.substringBeforeLast(" ")
-                } else {
-                    driverName
-                }
-
-                Text(
-                    text = trimmedDriverName,
-                    fontFamily = antaFamily,
-                    textAlign = TextAlign.Center,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 2,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                    style = MaterialTheme.typography.titleMedium
-                )
-
-                Spacer(modifier = Modifier.width(8.dp))
-
-                // To peacefully accommodate german names which only consists of two words
-                val codriverName = result.team.codriver
-                val trimmedCodriverName = if (codriverName.split(" ").size > 2) {
-                    codriverName.substringBeforeLast(" ")
-                } else {
-                    codriverName
-                }
-
-                Text(
-                    text = trimmedCodriverName,
-                    fontFamily = antaFamily,
-                    overflow = TextOverflow.Ellipsis,
-                    textAlign = TextAlign.Center,
-                    maxLines = 2,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer,
-                    style = MaterialTheme.typography.titleMedium
-                )
-
-                Spacer(modifier = Modifier.height(8.dp))
-
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(vertical = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Icon(
-                        painterResource(id = R.drawable.timer_outlined),
-                        contentDescription = null
-                    )
-
-                    Spacer(modifier = Modifier.width(4.dp))
-
-                    Text(
-                        text = result.time,
-                        fontFamily = antaFamily,
-                        fontWeight = FontWeight.Bold,
-                        style = MaterialTheme.typography.titleLarge
-                    )
-                }
-
-                Box(
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .border(
-                            width = 2.dp,
-                            color = MaterialTheme.colorScheme.onSurface,
-                            shape = RoundedCornerShape(2.dp)
+                Column {
+                    Box(
+                        modifier = Modifier.fillMaxHeight()
+                    ) {
+                        Text(
+                            text = position.toString(),
+                            fontFamily = antaFamily,
+                            style = MaterialTheme.typography.headlineMedium,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer,
+                            modifier = Modifier
+                                .padding(start = 8.dp)
+                                .align(Alignment.Center)
                         )
-                        .background(
-                            color = MaterialTheme.colorScheme.onPrimary,
-                            shape = RoundedCornerShape(2.dp)
-                        ),
-                    contentAlignment = Alignment.Center,
+
+                        Text(
+                            text = result.team.category.categoryId.substring(0, 5).uppercase(),
+                            style = MaterialTheme.typography.bodyMedium,
+                            fontFamily = antaFamily,
+                            color = MaterialTheme.colorScheme.onPrimaryContainer.copy(alpha = 0.5f),
+                            modifier = Modifier
+                                .padding(start = 8.dp)
+                                .align(Alignment.BottomCenter),
+                        )
+                    }
+                }
+
+                /*VerticalDivider(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .width(4.dp)
+                        .padding(horizontal = 8.dp),
+                    thickness = 4.dp,
+                    color = MaterialTheme.colorScheme.primary
+                )*/
+
+                // Column with three Texts
+                Column(
+                    modifier = Modifier
+                        .weight(0.8f)
+                        .padding(start = 8.dp, top = 16.dp, end = 8.dp, bottom = 16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
+                    // To peacefully accommodate german names which only consists of two words
+                    val driverName = result.team.driver
+                    val trimmedDriverName = if (driverName.split(" ").size > 2) {
+                        driverName.substringBeforeLast(" ")
+                    } else {
+                        driverName
+                    }
 
                     Text(
-                        text = "#" + result.number,
-                        color = MaterialTheme.colorScheme.onSurface,
-                        style = MaterialTheme.typography.titleMedium,
-                        fontFamily = ezraFamily,
-                        modifier = Modifier.padding(start = 4.dp, end = 4.dp, top = 4.dp)
+                        text = trimmedDriverName,
+                        fontFamily = antaFamily,
+                        textAlign = TextAlign.Center,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 2,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        style = MaterialTheme.typography.titleMedium
                     )
+
+                    Spacer(modifier = Modifier.width(8.dp))
+
+                    // To peacefully accommodate german names which only consists of two words
+                    val codriverName = result.team.codriver
+                    val trimmedCodriverName = if (codriverName.split(" ").size > 2) {
+                        codriverName.substringBeforeLast(" ")
+                    } else {
+                        codriverName
+                    }
+
+                    Text(
+                        text = trimmedCodriverName,
+                        fontFamily = antaFamily,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center,
+                        maxLines = 2,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Icon(
+                            painterResource(id = R.drawable.timer_outlined),
+                            contentDescription = null
+                        )
+
+                        Spacer(modifier = Modifier.width(4.dp))
+
+                        Text(
+                            text = result.time,
+                            fontFamily = antaFamily,
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.titleLarge
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier
+                            .wrapContentWidth()
+                            .border(
+                                width = 2.dp,
+                                color = MaterialTheme.colorScheme.onSurface,
+                                shape = RoundedCornerShape(2.dp)
+                            )
+                            .background(
+                                color = if (result.disqualified) MaterialTheme.colorScheme.error
+                                else MaterialTheme.colorScheme.onPrimary,
+                                shape = RoundedCornerShape(2.dp)
+                            ),
+                        contentAlignment = Alignment.Center,
+                    ) {
+
+                        Text(
+                            text = "#" + result.number,
+                            color = if (result.disqualified) MaterialTheme.colorScheme.onError
+                            else MaterialTheme.colorScheme.onSurface,
+                            style = MaterialTheme.typography.titleMedium,
+                            fontFamily = ezraFamily,
+                            modifier = Modifier.padding(start = 4.dp, end = 4.dp, top = 4.dp)
+                        )
+                    }
                 }
+
+                Log.d("ResultCard", "Image URL: ${result.team.driverImage}")
+
+                DriverImagesPager(result = result)
             }
-
-            Log.d("ResultCard", "Image URL: ${result.team.driverImage}")
-
-            DriverImagesPager(result = result)
         }
     }
 }
