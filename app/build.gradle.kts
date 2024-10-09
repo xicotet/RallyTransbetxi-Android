31991e00ef48ce7c057f5b9bfaad02806cb3c664
xicotet <pablocanofranch@gmail.com>
2024-10-09 13:27:15 +0200

    Update app version to 0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1"
 
         val properties = Properties()
         properties.load(project.rootProject.file("local.properties").inputStream())
