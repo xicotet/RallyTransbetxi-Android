aa8410d3539102a8027deb3279cebcfb37c10b12
xicotet <pablocanofranch@gmail.com>
2024-10-19 19:51:59 +0200

    Update branding logo size in splash screen

--- app/src/main/java/com/canolabs/rallytransbetxi/ui/onboarding/OnboardingFlow.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/onboarding/OnboardingFlow.kt
@@ -32,7 +32,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/canolabs/rallytransbetxi/ui/results/ResultsCard.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/results/ResultsCard.kt
@@ -1,6 +1,5 @@
 package com.canolabs.rallytransbetxi.ui.results
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
