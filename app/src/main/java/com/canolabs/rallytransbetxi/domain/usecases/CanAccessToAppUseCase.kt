9577ef10128a984663a51febaca0e4cf24f4ccfc
xicotet <pablocanofranch@gmail.com>
2024-11-12 11:08:01 +0100

    Add fallback to allow access if version fetch fails

--- app/src/main/java/com/canolabs/rallytransbetxi/domain/usecases/CanAccessToAppUseCase.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/domain/usecases/CanAccessToAppUseCase.kt
@@ -1,5 +1,6 @@
 package com.canolabs.rallytransbetxi.domain.usecases
 
+import android.util.Log
 import com.canolabs.rallytransbetxi.data.repositories.AppVersionRepositoryImpl
 import javax.inject.Inject
 
@@ -9,6 +10,12 @@ class CanAccessToAppUseCase @Inject constructor(
     suspend operator fun invoke(): Boolean {
         val currentVersion = appVersionRepositoryImpl.getCurrentVersion()
         val minAllowedVersion = appVersionRepositoryImpl.getMinAllowedVersion()
+
+        // Guarantee access if for some reason version fetch fails (i.e., returns [0, 0, 0])
+        if (currentVersion == listOf(0, 0, 0) || minAllowedVersion == listOf(0, 0, 0)) {
+            Log.w("CanAccessToAppUseCase", "Version fetch failed. Allowing access.")
+            return true
+        }
 
         return currentVersion.zip(minAllowedVersion).any { (currentPart, minPart) ->
             currentPart != minPart && currentPart > minPart
