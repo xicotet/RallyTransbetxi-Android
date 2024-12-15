d0f79fd9ee70423a699b54c0d5561fe45ae84b3b
xicotet <pablocanofranch@gmail.com>
2024-12-15 19:37:20 +0100

    Add custom info window over start/end stage markers

--- app/src/main/java/com/canolabs/rallytransbetxi/ui/maps/MapContent.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/maps/MapContent.kt
@@ -17,7 +17,13 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.util.Log
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
@@ -25,6 +31,8 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.material3.AssistChip
 import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.BottomSheetDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -49,6 +57,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -71,6 +81,7 @@ import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapType
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerInfoWindow
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.Polyline
 import kotlinx.coroutines.delay
@@ -145,7 +156,9 @@ fun MapContent(
         Box(modifier = Modifier.fillMaxSize()) {
             GoogleMap(
                 modifier = Modifier.fillMaxSize(),
-                uiSettings = state.uiSettings,
+                uiSettings = state.uiSettings.copy(
+                    mapToolbarEnabled = false,
+                ),
                 properties = state.mapProperties,
                 onMapLoaded = {
                     isMapReady.value = true
@@ -160,28 +173,69 @@ fun MapContent(
                     color = MaterialTheme.colorScheme.primary
                 )
 
+                var isStartMarkerInfoWindowVisible by remember { mutableStateOf(false) }
+                var isEndMarkerInfoWindowVisible by remember { mutableStateOf(false) }
+
                 // Start point marker
                 if (startMarkerIcon.value != null && isMapReady.value) {
-                    Marker(
+                    MarkerInfoWindow(
                         state = MarkerState(state.stage.geoPoints?.first()?.let {
                             LatLng(it.latitude, it.longitude)
                         } ?: betxi),
-                        title = state.stage.name,
                         flat = true,
-                        icon = startMarkerIcon.value!!
-                    )
+                        icon = startMarkerIcon.value!!,
+                        infoWindowAnchor = Offset(x = 0.5f, y = 4f),
+                        onInfoWindowClick = {
+                            val latLng = state.stage.geoPoints?.first()?.let {
+                                LatLng(it.latitude, it.longitude)
+                            }
+                            val uri = Uri.parse("geo:${latLng?.latitude},${latLng?.longitude}?q=${latLng?.latitude},${latLng?.longitude}")
+                            val intent = Intent(Intent.ACTION_VIEW, uri).apply {
+                                setPackage("com.google.android.apps.maps")
+                            }
+                            context.startActivity(intent)
+                        },
+                        onClick = {
+                            isEndMarkerInfoWindowVisible = false
+                            isStartMarkerInfoWindowVisible = !isStartMarkerInfoWindowVisible
+                            false
+                        }
+                    ) {
+                        if (isStartMarkerInfoWindowVisible) {
+                            CustomInfoWindow(text = state.stage.name.substringBefore("-"))
+                        }
+                    }
                 }
 
                 // End point marker
                 if (endMarkerIcon.value != null && isMapReady.value) {
-                    Marker(
+                    MarkerInfoWindow(
                         state = MarkerState(state.stage.geoPoints?.last()?.let {
                             LatLng(it.latitude, it.longitude)
                         } ?: betxi),
-                        title = state.stage.name,
                         flat = true,
+                        infoWindowAnchor = Offset(x = 0.5f, y = 4f),
+                        onInfoWindowClick = {
+                            val latLng = state.stage.geoPoints?.first()?.let {
+                                LatLng(it.latitude, it.longitude)
+                            }
+                            val uri = Uri.parse("geo:${latLng?.latitude},${latLng?.longitude}?q=${latLng?.latitude},${latLng?.longitude}")
+                            val intent = Intent(Intent.ACTION_VIEW, uri).apply {
+                                setPackage("com.google.android.apps.maps")
+                            }
+                            context.startActivity(intent)
+                        },
+                        onClick = {
+                            isStartMarkerInfoWindowVisible = false
+                            isEndMarkerInfoWindowVisible = !isEndMarkerInfoWindowVisible
+                            false
+                        },
                         icon = endMarkerIcon.value!!
-                    )
+                    ) {
+                        if (isEndMarkerInfoWindowVisible) {
+                            CustomInfoWindow(text = state.stage.name.substringAfter("-"))
+                        }
+                    }
                 }
 
                 PrintUserLocation(location = state.location)
@@ -515,6 +569,67 @@ fun MapContent(
                     }
                 )
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun CustomInfoWindow(
+    text: String,
+) {
+    Column(
+        modifier = Modifier
+            .background(
+                color = MaterialTheme.colorScheme.surface.copy(alpha = 0.9f),
+                shape = RoundedCornerShape(32.dp)
+            )
+            .padding(16.dp)
+            .wrapContentSize()
+            .clip(RoundedCornerShape(32.dp)),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = text,
+            fontFamily = robotoFamily,
+            style = MaterialTheme.typography.titleMedium
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                // Already handled in the MarkerInfoWindow
+            },
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .padding(horizontal = 8.dp)
+                .clip(RoundedCornerShape(50)),
+            contentPadding = PaddingValues(4.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primary
+            )
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(32.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.onPrimary)
+                    .padding(4.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.google_maps_icon),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    tint = Color.Unspecified
+                )
+            }
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = stringResource(id = R.string.open_in_google_maps),
+                modifier = Modifier.basicMarquee(initialDelayMillis = 1000),
+                fontFamily = robotoFamily,
+                maxLines = 1,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
