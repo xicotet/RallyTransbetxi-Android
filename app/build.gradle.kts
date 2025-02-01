aaa3ebe197a82870c513e1d135f5934a9f70ff61
xicotet <pablocanofranch@gmail.com>
2025-02-01 13:36:27 +0100

    Update app version to 0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.canolabs.rallytransbetxi"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
 
         val properties = Properties()
         properties.load(project.rootProject.file("local.properties").inputStream())
