5f2ff661f2634abb9df342ccaee92fc470969b15
xicotet <pablocanofranch@gmail.com>
2025-02-19 14:06:33 +0100

    Update animation transitions duration

--- app/src/main/java/com/canolabs/rallytransbetxi/ui/navigation/NavigationTransitions.kt
+++ app/src/main/java/com/canolabs/rallytransbetxi/ui/navigation/NavigationTransitions.kt
@@ -1,6 +1,6 @@
 package com.canolabs.rallytransbetxi.ui.navigation
 
-import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
@@ -10,36 +10,36 @@ class NavigationTransitions {
 
     companion object {
         val enterTransition =
-            slideIn(tween(400, easing = LinearEasing)) { fullSize ->
+            slideIn(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
                 IntOffset(fullSize.width, 0)
             }
 
-        val exitTransition = slideOut(tween(400, easing = LinearEasing)) { fullSize ->
+        val exitTransition = slideOut(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
             IntOffset(-(fullSize.width), 0)
         }
 
-        val popEnterTransition = slideIn(tween(400, easing = LinearEasing)) { fullSize ->
+        val popEnterTransition = slideIn(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
             IntOffset(-(fullSize.width), 0)
         }
 
-        val popExitTransition = slideOut(tween(400, easing = LinearEasing)) { fullSize ->
+        val popExitTransition = slideOut(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
             IntOffset(fullSize.width, 0)
         }
 
         val enterFromBottomTransition =
-            slideIn(tween(400, easing = LinearEasing)) { fullSize ->
+            slideIn(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
                 IntOffset(0, fullSize.height)
             }
 
-        val exitToBottomTransition = slideOut(tween(400, easing = LinearEasing)) { fullSize ->
+        val exitToBottomTransition = slideOut(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
             IntOffset(0, -fullSize.height)
         }
 
-        val popEnterFromBottomTransition = slideIn(tween(400, easing = LinearEasing)) { fullSize ->
+        val popEnterFromBottomTransition = slideIn(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
             IntOffset(0, -fullSize.height)
         }
 
-        val popExitToBottomTransition = slideOut(tween(400, easing = LinearEasing)) { fullSize ->
+        val popExitToBottomTransition = slideOut(tween(300, easing = FastOutSlowInEasing)) { fullSize ->
             IntOffset(0, fullSize.height)
         }
     }
